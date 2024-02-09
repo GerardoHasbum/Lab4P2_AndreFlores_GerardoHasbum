39df1d6ee6af09193a401225fd6dc009dfaee851
GerardoHasbum <ghasbumgo@gmail.com>
2024-02-09 15:17:31 -0600

    Validacion de login Gerardo

--- src/lab4p2_andreflores_gerardohasbum/Lab4P2_AndreFlores_GerardoHasbum.java
+++ src/lab4p2_andreflores_gerardohasbum/Lab4P2_AndreFlores_GerardoHasbum.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 public class Lab4P2_AndreFlores_GerardoHasbum {
 
+    static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         ArrayList<Usuarios> usuarios = new ArrayList();
@@ -13,14 +15,21 @@ public class Lab4P2_AndreFlores_GerardoHasbum {
         usuarios.add(new Usuarios("gerardo", "gerardoH@gmail.com", "clave123", 3000, "gerardo", 18));
         usuarios.add(new Usuarios("jack", "jack@gmail.com", "jack123", 3000, "jack", 18));
         int inicioSesion;
+        PrintA(usuarios);
+        Usuarios principal = iniciarSesion(usuarios);
+        while (principal == null) {
+
+            System.out.println("Ese usuario no existe favor intentar de nuevo");
+            principal = iniciarSesion(usuarios);
+
+        }
 
     }
 
-    private static Usuarios iniciarSesion(ArrayList<Usuarios> usuarios, Scanner entrada) {
+    private static Usuarios iniciarSesion(ArrayList<Usuarios> usuarios) {
         System.out.println("Inicio de sesion:");
         System.out.print("Nombre de usuario: ");
         String nombreUsuario = entrada.nextLine();
-        nombreUsuario = entrada.nextLine();
         System.out.print("Contrasenia: ");
         String contrasenia = entrada.nextLine();
 
@@ -30,6 +39,16 @@ public class Lab4P2_AndreFlores_GerardoHasbum {
             }
         }
         return null;
+    }
+
+    public static void PrintA(ArrayList x) {
+
+        for (int i = 0; i < x.size(); i++) {
+
+            System.out.println(i + ".- " + x.get(i).toString());
+
+        }
+
     }
 
 }
