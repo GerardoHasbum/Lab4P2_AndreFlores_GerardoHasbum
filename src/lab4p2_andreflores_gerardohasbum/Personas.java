944f80eae347709937d0c5c5fb2f46c66e084874
GerardoHasbum <ghasbumgo@gmail.com>
2024-02-09 14:42:14 -0600

    Usuarios y personas Gerardo

---
+++ src/lab4p2_andreflores_gerardohasbum/Personas.java
@@ -0,0 +1,16 @@
+package lab4p2_andreflores_gerardohasbum;
+
+public class Personas {
+
+    private String nombre;
+    private int edad;
+
+    public Personas() {
+    }
+    
+    public Personas(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+}
---
+++ src/lab4p2_andreflores_gerardohasbum/Usuarios.java
@@ -0,0 +1,60 @@
+package lab4p2_andreflores_gerardohasbum;
+
+import java.util.ArrayList;
+
+public class Usuarios extends Personas{
+
+    private String usuario, correo, password;
+    double dinero;
+    ArrayList agentes = new ArrayList();
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String usuario, String correo, String password, double dinero, String nombre, int edad) {
+        super(nombre, edad);
+        this.usuario = usuario;
+        this.correo = correo;
+        this.password = password;
+        this.dinero = dinero;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(double dinero) {
+        this.dinero = dinero;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios{" + "usuario=" + usuario + ", correo=" + correo + ", password=" + password + ", dinero=" + dinero + ", agentes=" + agentes + '}';
+    }
+    
+    
+}
